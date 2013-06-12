9afe7ef891f6ba56b104014a50986bd83bfe6a03
Jordan Halterman <jordan.halterman@gmail.com>
2013-06-12 18:52:56 -0500

    Add basic setup script and module.

---
+++ setup.py
@@ -0,0 +1,26 @@
+__version__ = '0.1.0'
+
+from setuptools import setup
+
+if __name__ == '__main__':
+  setup(
+    name='Extreme Python',
+    description='Takes Python programming to the extreme!',
+    author='Jordan Halterman',
+    version=__version__,
+    author_email='jordan.halterman@gmail.com',
+    url='https://github.com/jordanhalterman/extreme-python',
+    keywords=['extreme', 'oop'],
+    py_modules=['extreme'],
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Environment :: Console',
+      'Intended Audience :: Developers',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 2.5',
+      'Programming Language :: Python :: 2.6',
+      'Programming Language :: Python :: 2.7',
+      'Natural Language :: English',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+    ])
